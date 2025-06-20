8bb5a9deca0abcbd5c0b4f08ba521e15777684b9
CrisVatca <95505567+CrisVatca@users.noreply.github.com>
2025-06-20 12:04:04 +0300

    Update docusaurus.config.ts

--- my-wiki/docusaurus.config.ts
+++ my-wiki/docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
     v4: true,
   },
 
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'http://happybirthdayinferno.xyz',
   baseUrl: '/',
 
   organizationName: 'facebook',
